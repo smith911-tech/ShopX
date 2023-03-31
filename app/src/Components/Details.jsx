3d17abbe21d846140368e96bbc4f010ab6fd2480
joseph williams <wj882244@gmail.com>
2023-03-31 17:32:23 +0100

    added scroll

--- app/src/Components/Details.jsx
+++ app/src/Components/Details.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 import Data from '../Components/Data file/Data'
 import { useParams } from "react-router-dom"
 export default function Details() {
@@ -10,6 +10,21 @@ export default function Details() {
     setData(data);
     }
     }, [id]);
+    const handleImageClick1 = () => {
+    document.getElementById("myid1").scrollIntoView({ behavior: "smooth" });
+    };
+    const handleImageClick2 = () => {
+    document.getElementById("myid2").scrollIntoView({ behavior: "smooth" });
+    };
+    const handleImageClick3 = () => {
+    document.getElementById("myid3").scrollIntoView({ behavior: "smooth" });
+    };
+    const handleImageClick4 = () => {
+    document.getElementById("myid4").scrollIntoView({ behavior: "smooth" });
+    };
+    const handleImageClick5 = () => {
+    document.getElementById("myid5").scrollIntoView({ behavior: "smooth" });
+    };
     return(
         <>
         <div>
@@ -20,23 +35,25 @@ export default function Details() {
         <main className="padding-main-details">
             <div className="div-img-carrier-details-page">
                 <aside className="aside-img-clickable">
-                <a href="#img1"><img src={data.img} alt="img" /></a>
-                <a href="#img2"><img src={data.img2} alt="img" /></a>
-                <a href="#img3"><img src={data.img3} alt="img" /></a>
-                <a href="#img4"><img src={data.img4} alt="img" /></a>
-                <a href="#img5"><img src={data.img5} alt="img" /></a>
+                <img onClick={handleImageClick1} src={data.img} alt="img" />
+                <img onClick={handleImageClick2} src={data.img2} alt="img" />
+                <img onClick={handleImageClick3} src={data.img3} alt="img" />
+                <img onClick={handleImageClick4} src={data.img4} alt="img" />
+                <img onClick={handleImageClick5} src={data.img5} alt="img" />
                 </aside>
                 <div className="flex-tagsimg">
-                <img id="img1" src={data.img} alt="img" />
-                <img id="img2" src={data.img2} alt="img" />
-                <img id="img3" src={data.img3} alt="img" />
-                <img id="img4" src={data.img4} alt="img" />
-                <img id="img5" src={data.img5} alt="img" />
+                <img id="myid1" src={data.img} alt="img" />
+                <img id="myid2" src={data.img2} alt="img" />
+                <img id="myid3" src={data.img3} alt="img" />
+                <img id="myid4" src={data.img4} alt="img" />
+                <img id="myid5" src={data.img5} alt="img" />
                 </div>
             </div>
             <aside className="namedescprice-details">
                 <h3  className="namedata-details">{data.name}</h3>
                 <h3  className="pricedata-details"><sup>$</sup>{data.price}</h3>
+                <hr />
+                <h3>Description:</h3>
                 <p className="descdata-details">{data.desc}</p>
             </aside>
 
