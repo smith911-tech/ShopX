6ec703031ae36e0a577445d33c63bb9fbb35bcb7
joseph williams <wj882244@gmail.com>
2023-04-04 09:41:22 +0100

    Update

--- app/src/Components/Cart.jsx
+++ app/src/Components/Cart.jsx
@@ -5,8 +5,23 @@ export default function Cart(){
         <>
             {
                 cartItems.map((item) => (
-                    <div key={item.id}>
-                    <h2>{item.name}</h2>
+                    <div className="flex-cart-page" key={item.id}>
+                    <img src={item.img} alt="" className="img-cartside"/>
+                    <div className="count-name-flex">
+                        <p className="namecart-page">{item.name}</p>
+                        <div className="flex-remove-count">
+                        <div>
+                        <p>Quantity:</p>
+                        <div className="count-cart">
+                            <div className="quantity-add"><h3>+</h3></div>
+                            <div className="count"><h3>4</h3></div>
+                            <div className="quantity-sub"><h3>-</h3></div>
+                        </div>
+                        </div>
+                    <p className="remove-cart">Remove</p>
+                        </div>
+                    </div>
+                    <h3 className="price-cart"><sup>$</sup>{item.price}</h3>
                     </div>
                 ))
             }
