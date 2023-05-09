2142c1add39a8cdaa26cc8ae40306269429bb45d
joseph williams <wj882244@gmail.com>
2023-05-09 11:32:38 +0100

    update

--- app/src/Components/SignUpA.jsx
+++ app/src/Components/SignUpA.jsx
@@ -1,31 +1,48 @@
 import { useNavigate } from "react-router-dom"
 import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import {auth} from "../firebase"
 export default function SignUpA() {
-    const [carryFullname, setcarryFullname] = useState(false)
-    const [carryEmail, setcarryEmail] = useState(false)
-    const [carrypassword, setcarrypassword] = useState(false)
-    const [carrypasswordC, setcarrypasswordC] = useState(false)
+    const [formData, setFormData] = useState({
+    fullname: "", 
+    email: "", 
+    password: "", 
+    passwordC: "" 
+})  
+    const [error, seterror] = useState("")
+    const [carrier, setcarrier] = useState(false)
     const [showpassword, setshowpassword] = useState(false)
     const navigate = useNavigate()
     function navigateSignIn(){
         navigate('/')
         window.scrollTo(0,0)
     }
-    function carryFullNameF(){
-        setcarryFullname(true)
-    }
-        function carryEmailF(){
-        setcarryEmail(true)
-    }
-        function carryPassF(){
-        setcarrypassword(true)
-    }
-        function carryPassCF(){
-        setcarrypasswordC(true)
+    function carrierF(){
+    setcarrier(true)
     }
         function showpassF() {
         setshowpassword(!showpassword)
     }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.email === '' || formData.password === '' || formData.confirmPassword === ''  || formData.fullname === '') {
+      seterror('Please fill in all fields');
+    } else if (formData.password !== formData.confirmPassword) {
+      seterror('Passwords do not match');
+    } else {
+      createUserWithEmailAndPassword(auth, formData.email, formData.password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          navigate('/Mainapp');
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    }
+  };
     return (
         <main className="full-signdiv">
         
@@ -35,61 +52,73 @@ export default function SignUpA() {
         <form action="" className="form0">
 
             <div className={`in-text ${
-            carryFullname ? "out-text" : "in-text"
+            carrier ? "out-text" : "in-text"
             }`}>
             <label htmlFor="name" className="afterFullName"></label>
             <input
             type="text" 
             name="" 
+            value={formData.fullname}
             id="name"  
-            onClick={carryFullNameF}
-            onChange={carryFullNameF}
+            onClick={carrierF}
+            
+            required
             /></div>
 
             <div className={`in-text 
-            ${carryEmail ? "out-text" : "in-text"}`}>
+            ${carrier ? "out-text" : "in-text"}`}>
             <label htmlFor="" className="afterEmail"></label>
             <input 
             type="email"
              name=""
             id="" 
-            onClick={carryEmailF}
-            onChange={carryEmailF}
+            value={formData.email}
+            onClick={carrierF}
+            
+            required
             />
             </div>
             
             <div className={`in-text 
-            ${carrypassword ? "out-text" : "in-text"}`}>
+            ${carrier ? "out-text" : "in-text"}`}>
             <label htmlFor="Password" className="afterPassword"></label>
             <input
             type={showpassword ? "text" : "password"}
             name="password" 
             id="Password"  
-            onClick={carryPassF}
-            onChange={carryPassF}
+            value={formData.password}
+            onClick={carrierF}
+            
+            required
             /></div>
 
             <div className={`in-text 
-            ${carrypasswordC ? "out-text" : "in-text"}`}>
+            ${carrier ? "out-text" : "in-text"}`}>
             <label htmlFor="" className="afterConfirmPassword"></label>
             <input 
             type={showpassword ? "text" : "password"} 
             name="password" 
-            onChange={carryPassCF}
-            onClick={carryPassCF}
+            value={formData.passwordC}
+            onClick={carrierF}
+            
             id=""  
+            required
             /></div>
             
             <div className="password-check">
             <label htmlFor="showpass" className="showpassword"></label>
             <input type="checkbox" name="" id="showpass" onClick={showpassF}/>
             </div>
+            
+            {error && <p className="errorsignup">{error}</p>}
 
-            <button className="signBtn" >Sign Up</button>
+        <button className="signBtn" onClick={handleSubmit}>Sign Up</button>
 
             <p className="newusertell">Have an Account ? <span className="signupLogin" onClick={navigateSignIn}>Sign In</span></p>
 
         </form>
+
+
         </div>
         </main>
     )
--- app/src/firebase.js
+++ app/src/firebase.js
@@ -18,4 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export default app;
